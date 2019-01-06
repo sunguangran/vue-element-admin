fc758f810093844fae229e87744e34cb06c77bc5
sunguangran <sunguangran@daixiaomi.com>
2019-01-06 21:53:14 +0800

    添加pandora模块

--- apis/__init__.py
+++ apis/__init__.py
@@ -2,4 +2,4 @@
 # Author: sunguangran (sunguangran@daixiaomi.com)
 from pynavi.view.request.url_refactor import url_patterns_maker
 
-pandora_patterns = url_patterns_maker(module=__name__, manager='^pandora1/manager/', default='^pandora1/')
+pandora_patterns = url_patterns_maker(module=__name__, manager='^manager/', default='^pandora/')
