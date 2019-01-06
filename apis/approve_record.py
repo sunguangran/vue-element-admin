3fc23ce9d5aecba2b9f9129bf9069e7a309ba01d
sunguangran <sunguangran@daixiaomi.com>
2019-01-06 14:06:04 +0800

    添加pandora模块

--- apis/approve_record.py
+++ apis/approve_record.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 # Author: sunguangran (sunguangran@daixiaomi.com)
+import json
+
+from django.http import HttpResponse
 from pynavi.view.request.url_refactor import url
 
 
 @url(r'^record/info')
 def get_record_info(request):
-    return {"a": 1}
+    # return {"a": 1}
+    return HttpResponse(json.dumps({"a": 1}), content_type="application/json")
